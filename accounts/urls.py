6f14689004c78e7550b4c71e25b82af7dbcb121a
moinul75 <moinul1955198@gmail.com>
2023-10-06 01:19:12 +0600

    implemented login auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from accounts.views import Home
+from accounts.views import Home,LoginView
 
 urlpatterns = [
     path('',Home.as_view(),name='home'),
+    path('login',LoginView.as_view(),name='login')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.views import generic 
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class Home(generic.TemplateView):
+class Home(LoginRequiredMixin, generic.TemplateView):
+    login_url = 'login'
     template_name = 'accounts/home.html'
+
+
+#login 
+class LoginView(generic.View):
+    def get(self,*args, **kwargs):
+        return render(self.request, 'accounts/login.html')
+        
+    
+    def post(self,*args, **kwargs):
+        pass 
+    
